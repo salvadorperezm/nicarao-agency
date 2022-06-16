d8c5f78c5305cf3a88f6125ef691da2db7ab1dd8
Salvador Perez <salvadorperezm115@gmail.com>
2022-06-16 16:37:56 -0600

    agregando la funcion que recupera las peliculas en tendencia

--- curso-practico-consumo-api/src/main.js
+++ curso-practico-consumo-api/src/main.js
@@ -1,1 +1,11 @@
+async function getTrendingMoviewsPreview() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
+  );
+  const data = await res.json();
 
+  const movies = data.results;
+  console.log({ data, movies });
+}
+
+getTrendingMoviewsPreview();
